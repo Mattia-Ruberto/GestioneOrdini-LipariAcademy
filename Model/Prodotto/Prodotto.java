c3c10334d4fdf2cb4298b2748490d2af1a428354
Mattia-Ruberto <mattia.ruberto@liparipeople.com>
2023-12-07 15:46:10 +0100

    Prodotto
    Implementazione della classe Prodotto

---
+++ Model/Prodotto/Prodotto.java
@@ -0,0 +1,62 @@
+package Prodotto;
+import java.util.UUID;
+
+public class Prodotto {
+	private Integer id;
+	private String code;
+	private String description;
+	private Double price;
+	private Integer quantity;
+	
+	public Prodotto(Integer id, String description, Double price, Integer qty) {
+		super();
+		this.id = id;
+		this.code = UUID.randomUUID().toString();
+		this.description = description;
+		this.price = price;
+		this.quantity = qty;
+	}
+	
+	public Integer getId() {
+		return this.id;
+	}
+	
+	public String getUUID() {
+		return this.code;
+	}
+	
+	public String getDescription() {
+		return this.description;
+	}
+	
+	public Double getPrice() {
+		return this.price;
+	}
+	
+	public Integer getQuantity() {
+		return this.quantity;
+	}
+	
+	public void addItem(int qty) {
+		this.quantity += qty;
+	}
+	
+	public void removeItem(int qty) {
+		if(this.quantity == 0) {
+			throw new ArithmeticException("Field quantity cannot contain negative numbers.");
+		}else {
+			this.quantity -= qty;
+		}
+	}
+	
+	public void updateDescription(String newDesc) {
+		this.description = newDesc;
+	}
+	
+	public void updatePrice(double newprice) {
+		this.price = newprice;
+	}
+}
+
+
+
